fa23cdc1419b11371d5880c19afc5404568e992b
Tianyuan Chen <ty.lostll@gmail.com>
2024-03-20 12:58:39 -0600

    initial commit

---
+++ flexible-keyboard/flexible-keyboard Watch App/flexible_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  flexible_keyboardApp.swift
+//  flexible-keyboard Watch App
+//
+//  Created by Tianyuan Chen on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct flexible_keyboard_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
